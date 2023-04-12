4ef22ed150f966a5e106b16206e82a83546ee2bb
alijprof <95873824+alijprof@users.noreply.github.com>
2023-04-12 23:56:46 +0100

    Update Consensus-Node.sh

--- Consensus-Node.sh
+++ Consensus-Node.sh
@@ -31,33 +31,27 @@ make build
 make install 
 make cel-key 
 
-#Setup the P2P networks
-cd $HOME
+# Setup the P2P networks
+cd "$HOME"
 rm -rf networks
 git clone https://github.com/celestiaorg/networks.git
-cd ~/celestia-app
+cd "$HOME/celestia-app"
 
-#Pick Name
-
-print("Do you want to a rubbish boring name or enter your own name?")
-user_choice = input("Enter '1' for boring name or '2' for own name: ")
-
-if user_choice == "1":
-then
-    NODE_NAME=$"Lazy Node Runner"
-    echo NODE_NAME=$NODE_NAME
+# Choose node name
+echo "Do you want to choose a name for your node? Enter '1' for a default name or '2' to enter your own name."
+read -r USER_CHOICE
+if [ "$USER_CHOICE" = "1" ]; then
+  NODE_NAME="Lazy Node Runner"
+else
+  read -p "Enter Your Name: " NODE_NAME
 fi
-if user_choice == "2":
-then
-    read -p "Enter Your Name: " NODE_NAME
-    echo NODE_NAME=$NODE_NAME
-fi
+echo "Node name: $NODE_NAME"
 
-#initialise Node
-celestia-appd init $NODE_NAME --chain-id blockspacerace-0
+# Initialize node
+celestia-appd init "$NODE_NAME" --chain-id blockspacerace-0
 
-#Copy Genesis.json file
-cp $HOME/networks/blockspacerace/genesis.json $HOME/.celestia-app/config
+# Copy genesis.json file
+cp "$HOME/networks/blockspacerace/genesis.json" "$HOME/.celestia-app/config"
 
 #Peers
 PERSISTENT_PEERS="be935b5942fd13c739983a53416006c83837a4d2@178.170.47.171:26656,cea09c9ac235a143d4b6a9d1ba5df6902b2bc2bd@95.214.54.28:20656,5c9cfba00df2aaa9f9fe26952e4bf912e3f1e8ee@195.3.221.5:26656,7b2f4cb70f04f2e9befb6ace66ce1ac7b3bea5b4@178.239.197.179:26656,7ee2ba21197d58679cfc1517b5bbc6465bed387a@65.109.67.25:26656,dc0656ab58280d641c8d10311d86627255bec8a1@148.251.85.27:26656,ccbd6262d0324e2e858594b639f4296cc4952c93@13.57.127.89:26656,a507b2bda6d2974c84ae1e8a8b788fc9e44d01f7@142.132.131.184:26656,9768290c60a746ee97ef1a5bcb8bee69066475e8@65.109.80.150:2600"
@@ -87,8 +81,7 @@ wget -O - https://snaps.qubelabs.io/celestia/${SNAP_NAME} | tar xf - \
     -C ~/.celestia-app/data/
    
 #create system service
-sudo tee <<EOF >/dev/null /etc/systemd/system/celestia-appd.service
-[Unit]
+echo "[Unit]
 Description=celestia-appd Cosmos daemon
 After=network-online.target
 [Service]
@@ -98,8 +91,8 @@ Restart=on-failure
 RestartSec=3
 LimitNOFILE=4096
 [Install]
-WantedBy=multi-user.target
-EOF
+WantedBy=multi-user.target" > $HOME/celestia-appd.service
+sudo mv $HOME/celestia-appd.service /etc/systemd/system/
 
 #enable and start service
 sudo systemctl enable celestia-appd
