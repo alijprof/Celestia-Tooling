fa0b8a034d91713ca82e94c54496624673eb0004
alijprof <95873824+alijprof@users.noreply.github.com>
2023-05-14 16:41:43 +0200

    Update Go-Install-v1.20.3.sh

--- Go-Install-v1.20.3.sh
+++ Go-Install-v1.20.3.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-ver="1.20.3"
-cd $HOME
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
+GO_VERSION="1.20.3"
 
-echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
+cd $HOME
+wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$GO_VERSION.linux-amd64.tar.gz"
+rm "go$GO_VERSION.linux-amd64.tar.gz"
+
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
 
 # source the .bash_profile file
-source $HOME/.bash_profile
+source ~/.bash_profile
