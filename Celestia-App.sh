2a9881653dedd70f2040f9533c215e5c29b42395
alijprof <95873824+alijprof@users.noreply.github.com>
2023-05-14 16:52:23 +0200

    Update Celestia-App.sh

--- Celestia-App.sh
+++ Celestia-App.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+APP_VERSION="v0.13.0"
+
+cd $HOME
+rm -rf celestia-app
+git clone https://github.com/celestiaorg/celestia-app.git
+cd celestia-app/
+git checkout tags/$APP_VERSION -b $APP_VERSION
+make install
