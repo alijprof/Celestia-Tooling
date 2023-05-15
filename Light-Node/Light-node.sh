276ea0e4dd454288f7be3f4c0470065eb16debe2
alijprof <95873824+alijprof@users.noreply.github.com>
2023-05-15 17:22:33 +0200

    Update Light-node.sh

--- Light-Node/Light-node.sh
+++ Light-Node/Light-node.sh
@@ -12,98 +12,82 @@ if [ ! -f ~/.bash_profile ]; then
     fi
 fi
 
-sudo apt update && sudo apt upgrade -y	
+sudo apt update && sudo apt upgrade -y
 
-#install pre-reqs
+# Install prerequisites
 sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu -y
 
-#install go ver1.20.3 sh
+# Install Go v1.20.3
 wget "https://raw.githubusercontent.com/alijprof/Celestia-Tooling/main/Go-Install-v1.20.3.sh"
 chmod a+x Go-Install-v1.20.3.sh
 ./Go-Install-v1.20.3.sh
 
 source ~/.bash_profile
 
-# confirm install
+# Confirm Go installation
 if command -v go &> /dev/null; then
     echo "Go is installed and the PATH is set up correctly"
     go version
 else
-    echo "Go is not installed or the PATH is not set up correctly"
+    echo "ERROR in set up of Go"
 fi
 
-#NETWORK SELECTION
-echo -n "select network (1 - mocha testnet, 2 - blockspacerace, 3 - arabica devnet) > "
+# Network selection
+echo -n "Select network (1 - mocha testnet, 2 - blockspacerace, 3 - arabica devnet) > "
 read selectnetwork
 echo
-if test "$selectnetwork" == "1"
-then
+if test "$selectnetwork" == "1"; then
     NETWORK="mocha"
     echo "export NETWORK=$NETWORK" >> $HOME/.bash_profile
     source $HOME/.bash_profile
-fi
-if test "$selectnetwork" == "2"
-then
+elif test "$selectnetwork" == "2"; then
     NETWORK="blockspacerace"
     echo "export NETWORK=$NETWORK" >> $HOME/.bash_profile
     source $HOME/.bash_profile
-fi
-if test "$selectnetwork" == "3"
-then
+elif test "$selectnetwork" == "3"; then
     NETWORK="arabica"
     echo "export NETWORK=$NETWORK" >> $HOME/.bash_profile
     source $HOME/.bash_profile
 fi
 
-#INSTALL CELESTIA NODE
-#version based on network selection
+# Install Celestia node
 cd $HOME
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
 
-#select version tag based on network
-if [ "$NETWORK" == "mocha" ]
-then
+# Select version tag based on network
+if [ "$NETWORK" == "mocha" ]; then
     CELESTIA_VER="v0.6.4"
-elif [ "$NETWORK" == "blockspacerace" ]
-then
+elif [ "$NETWORK" == "blockspacerace" ]; then
     CELESTIA_VER="v0.9.4"
-elif [ "$NETWORK" == "arabica" ]
-then
+elif [ "$NETWORK" == "arabica" ]; then
     CELESTIA_VER="v0.7.1"
 fi
 
 echo "export CELESTIA_VER=$CELESTIA_VER" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
-#checkout appropriate version and build Celestia
+# Checkout appropriate version and build Celestia
 git checkout tags/$CELESTIA_VER
-make build 
-sudo make install				
+make build
+sudo make install
 make cel-key
 
-#INITIALISE AS LIGHT NODE
+# Initialize as a light node
 celestia light init --p2p.network $NETWORK
 
-#SETUP VARIABLES
+# Setup variables
 source $HOME/.bash_profile
 
-#select gRPC endpoint use default or enter custom
-if [ "$NETWORK" == "mocha" ]
-then
+# select gRPC endpoint use default or enter custom
+if [ "$NETWORK" == "mocha" ]; then
     GRPC_PROMPT="select gRPC endpoint (1 - default-https://rpc-mocha.pops.one, 2 - enter custom gRPC) > "
     DEFAULT_URL="https://rpc-mocha.pops.one"
-fi
-
-if [ "$NETWORK" == "blockspacerace" ]
-then
+elif [ "$NETWORK" == "blockspacerace" ]; then
     GRPC_PROMPT="select gRPC endpoint (1 - default-https://rpc-blockspacerace.pops.one/, 2 - enter custom gRPC) > "
     DEFAULT_URL="https://rpc-blockspacerace.pops.one"
-fi
-
-if [ "$NETWORK" == "arabica" ]
-then
+elif [ "$NETWORK" == "arabica" ]; then
     GRPC_PROMPT="select gRPC endpoint (1 - default-https://limani.celestia-devops.dev, 2 - enter custom gRPC) > "
     DEFAULT_URL="https://limani.celestia-devops.dev"
 fi
@@ -111,13 +95,10 @@ fi
 echo -n "$GRPC_PROMPT"
 read selectendpoint
 echo
-if test "$selectendpoint" == "1"
-then
+if [ "$selectendpoint" == "1" ]; then
     RPC_ENDPOINT="$DEFAULT_URL"
     echo "export RPC_ENDPOINT=$RPC_ENDPOINT" >> $HOME/.bash_profile
-fi
-if test "$selectendpoint" == "2"
-then
+elif [ "$selectendpoint" == "2" ]; then
     read -p "Enter Your Endpoint: " RPC_ENDPOINT
     echo "export RPC_ENDPOINT=$RPC_ENDPOINT" >> $HOME/.bash_profile
 fi
